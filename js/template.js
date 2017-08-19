ae3b92160197c9580b64c2d8ebd96fc7c3c874cb
Kristina <30473732+msKSOP@users.noreply.github.com>
2017-08-19 16:11:33 -0400

    Update template.js

--- js/template.js
+++ js/template.js
@@ -10,7 +10,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('../Portfolio to Print/Posters/collagebg-01.png');
+		$(".banner-image").backstretch('msksop.github.io/images/collagebg-01.png');
 		
 		// Fixed header
 		//-----------------------------------------------
